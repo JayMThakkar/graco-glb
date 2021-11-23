b34bd00017c5ac5a4b819cbf875e505f4b06ad92
jaythakkar-avataar <73527636+jaythakkar-avataar@users.noreply.github.com>
2021-11-23 12:50:38 +0530

    first commit

--- index.js
+++ index.js
@@ -158,6 +158,6 @@ app.get('/downloadFile', function (req, res) {
 });
 
 
-app.listen(3000, () => { // listen to the port
-    console.log("listening to port " + 3000);
+app.listen(process.env.PORT, () => { // listen to the port
+    console.log("listening to port " + process.env.PORT);
 });
